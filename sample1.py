1258ff97b11bcdcac165c36f649e90308a7c356b
rajkumarreddy46 <rajkumarreddy46@gmail.com>
2024-09-11 04:47:58 +0000

    changed 2 files at a time

--- sample1.py
+++ sample1.py
@@ -1,3 +1,3 @@
-rajkumarreddy46@personal-swift-review:~/swift-review/githubfiles/multiple-prs$ cat sample.py 
+ 
 # Program to print 'Hello, swift!'
-print('Hello, swift')
+print('Hello, swiftreview')
